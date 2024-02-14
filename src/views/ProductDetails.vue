37cbb6dba9b8643ecdbee8a76b27c986b5150da7
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-14 15:47:45 +0100

    add buttons and event listeners for product page

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,8 +1,43 @@
 <template>
-    <h1>Product details</h1>
+  <button @click="router.push({ name: 'catalog' })">Back to Catalog</button>
+  <div class="product">
+    <div class="product-image">
+      <img :src="selectedProduct.thumbnail" alt="" />
+    </div>
+    <div class="product-details">
+      <p>Brand: {{ selectedProduct.brand }}</p>
+      <p>Description: {{ selectedProduct.description }}</p>
+      <h2>Price: {{ selectedProduct.price }}</h2>
+      <button @click="addToCart">Add to cart</button>
+    </div>
+  </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
+import { productsStore } from '@/stores/products'
+import { useRoute, useRouter } from 'vue-router'
 
+const productStore = productsStore()
+const route = useRoute()
+const router = useRouter()
+
+const selectedProduct = computed(() => {
+  return productStore.products.find((item) => item.id === Number(route.params.id))
+})
+
+const addToCart = () => {
+    console.log('aaaaa')
+}
 
 </script>
+
+<style scoped>
+.product {
+  display: flex;
+  margin-top: 50px;
+}
+.product-image {
+  margin-right: 24px;
+}
+</style>
