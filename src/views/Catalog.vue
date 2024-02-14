ce99d204459c06a9a76583113fdfd2adcb584280
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-14 13:30:00 +0100

    create dynamic elements based on fetched data

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>Catalog</h1>
+  <div class="products-list">
+    <div class="product" v-for="product in productStore.products" :key="product.id">
+      <img :src="product.thumbnail" alt="" />
+      <h2>Brand: {{ product.brand }}</h2>
+      <p>Description: {{ product.description }}</p>
+      <p>Price: ${{ product.price }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
