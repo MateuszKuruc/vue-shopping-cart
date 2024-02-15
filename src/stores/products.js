430871dc8976183a1917518d37228b8816befe6e
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-15 09:12:46 +0100

    change addToCart method to keep track of quantity of items

--- src/stores/products.js
+++ src/stores/products.js
@@ -16,7 +16,12 @@ export const productsStore = defineStore({
         })
     },
     addToCart(product) {
-      this.cart.push(product)
+      const itemAlreadyAdded = this.cart.find((item) => item.id === product.id)
+      if (itemAlreadyAdded) {
+        itemAlreadyAdded.quantity += 1
+      } else {
+        this.cart.push({ ...product, quantity: 1 })
+      }
     },
     removeFromCart(id) {
       this.cart = this.cart.filter((item) => item.id !== id)
