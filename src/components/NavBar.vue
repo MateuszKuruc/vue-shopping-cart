fae054e49abaa4de3922d647f489278495045077
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-15 15:16:54 +0100

    add all products and categories router links to switch between views

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -15,6 +15,11 @@
   <div class="border-2 border-black h-[300px] w-[100%]">
     <img class="h-full w-full object-cover" src="/public/hero.png" alt="" />
   </div>
+  <div class="flex gap-4 justify-center py-12">
+    <router-link to="/">All products</router-link>
+    |
+    <router-link to="/categories">Categories</router-link>
+  </div>
 </template>
 
 <script setup>
