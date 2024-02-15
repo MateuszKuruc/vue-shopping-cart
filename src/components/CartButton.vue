9ccd7ebb4af4b6e03d1181bac2e21078b6890d02
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-15 19:11:27 +0100

    change button styles

--- src/components/CartButton.vue
+++ src/components/CartButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="py-4 px-8 mt-4 rounded-full uppercase font-bold border-2 border-black bg-gradient-to-r from-green-500 to-green-800 ease-in-out duration-700 hover:bg-gradient-to-r hover:from-blue-500 hover:via-indigo-400 hover:to-purple-600"
+    class="py-4 px-8 mt-4 rounded-full uppercase font-bold border-2 border-black bg-gradient-to-r from-green-500 to-green-800 ease-in-out duration-700 hover:bg-gradient-to-r hover:from-red-500 hover:via-yellow-400 hover:to-orange-600"
   >
     <slot>
       <span>Add to cart</span>
