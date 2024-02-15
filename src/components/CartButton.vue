4eef8ddb57cce464a1ddcb96f83230d2f47a293a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-15 20:40:43 +0100

    fix text color for navbar and buttons

--- src/components/CartButton.vue
+++ src/components/CartButton.vue
@@ -1,9 +1,9 @@
 <template>
   <button
-    class="py-4 px-8 mt-4 rounded-full uppercase font-bold border-2 border-black bg-gradient-to-r from-green-500 to-green-800 ease-in-out duration-700 hover:bg-gradient-to-r hover:from-red-500 hover:via-yellow-400 hover:to-orange-600 w-[200px]"
+    class="py-4 px-8 mt-4 rounded-full uppercase font-bold border-2 border-black bg-gradient-to-r from-green-500 to-green-800 hover:bg-gradient-to-r hover:from-red-500 hover:via-yellow-400 hover:to-orange-600 w-[200px] text-slate-200 hover:text-black"
   >
     <slot>
-      <span class="text-slate-200">Add to cart</span>
+      <span>Add to cart</span>
     </slot>
   </button>
 </template>
