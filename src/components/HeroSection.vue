c089feae428b334ce3c07feaf3049dbf904f5ba0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-16 12:15:54 +0100

    fix hero and ctalog sections for mobile screen

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,7 +2,7 @@
   <div class="h-[300px] w-[100%] mx-4 md:block hidden border-2 border-black rounded-xl">
     <img class="h-full w-full object-cover rounded-xl" src="/public/hero.png" alt="" />
   </div>
-  <div class="flex gap-4 justify-center md:py-12 md:pt-0 pt-[150px]">
+  <div class="flex gap-4 justify-center md:py-12 md:pt-0 pt-[120px]">
     <router-link to="/">All products</router-link>
     |
     <router-link to="/categories">Categories</router-link>
