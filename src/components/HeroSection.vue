34c92d92d08ac225d541c84b7ab91c2f9637afbb
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-16 11:58:57 +0100

    update hero section for mobile

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="h-[300px] w-[100%] mx-4 border-2 border-black rounded-xl">
+  <div class="h-[300px] w-[100%] mx-4 md:block hidden border-2 border-black rounded-xl">
     <img class="h-full w-full object-cover rounded-xl" src="/public/hero.png" alt="" />
   </div>
-  <div class="flex gap-4 justify-center py-12">
+  <div class="flex gap-4 justify-center md:py-12 md:pt-0 pt-[150px]">
     <router-link to="/">All products</router-link>
     |
     <router-link to="/categories">Categories</router-link>
