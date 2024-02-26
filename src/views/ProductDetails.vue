2b11e675fd14f5f1c3efbc45b865a293629b0ccf
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-27 00:36:55 +0100

    add pagination and navigation modules from swiper library and add carousel to product details comp

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -2,7 +2,17 @@
   <BackButton class="md:ml-8 ml-2" @click="router.go(-1)">Previous page</BackButton>
   <div v-if="displayedProduct" class="flex md:flex-row flex-col mt-12">
     <div class="md:ml-8 md:mr-24 md:w-[600px] mx-2">
-      <img class="rounded-xl max-h-[400px]" :src="displayedProduct.thumbnail" alt="" />
+      <Swiper
+        :modules="modules"
+        :slides-per-view="1"
+        :pagination="{ clickable: true }"
+        :navigation="true"
+        :grab-cursor="true"
+      >
+        <SwiperSlide v-for="photo in displayedProduct.images" :key="displayedProduct.id">
+          <img class="rounded-xl max-h-[400px]" :src="photo" alt="" />
+        </SwiperSlide>
+      </Swiper>
     </div>
     <div class="flex flex-col items-center justify-between md:mt-0 mt-4">
       <div class="flex flex-col md:gap-8 gap-4 md:mx-0 mx-4">
@@ -21,6 +31,12 @@ import { productsStore } from '@/stores/products'
 import { useRoute, useRouter } from 'vue-router'
 import BackButton from '@/components/BackButton.vue'
 import ConfirmButton from '@/components/ConfirmButton.vue'
+
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import 'swiper/swiper-bundle.css'
+import { Pagination, Navigation } from 'swiper/modules'
+
+const modules = [Pagination, Navigation]
 
 const productStore = productsStore()
 const route = useRoute()
