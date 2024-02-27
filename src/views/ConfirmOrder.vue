99e4a1d08ced9e93b97cb687f8d2218226f40287
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-27 16:12:59 +0100

    adjust confirmation page for mobile screen

--- src/views/ConfirmOrder.vue
+++ src/views/ConfirmOrder.vue
@@ -4,29 +4,29 @@
     <div class="text-center" v-if="!cart.length">
       <h1 class="text-3xl font-bold md:mt-0 mt-14">The cart is empty</h1>
     </div>
-    <div v-else class="flex flex-col w-[1600px] mt-8 md:pb-8 pb-8">
+    <div v-else class="flex flex-col xl:w-[1600px] md:w-auto mt-8 md:pb-8 pb-8">
       <div class="md:pb-2 pb-4" v-for="product in cart" :key="product.id">
         <div
-          class="flex flex-row shadow-2xl rounded-xl md:p-2 p-8 md:gap-0 mx-8 border-2 border-green-500"
+          class="flex flex-row shadow-2xl rounded-xl md:p-2 p-2 md:gap-0 md:mx-8 border-2 border-green-500"
         >
-          <div class="flex flex-row w-[100%] gap-8">
-            <p class="w-[300px]">
+          <div class="flex md:flex-row flex-col w-[100%] md:gap-8 gap-1">
+            <p class="md:w-[300px]">
               Item:
               <span class="font-bold">
                 {{ product.title }}
               </span>
             </p>
-            <p class="w-[300px]">
+            <p class="md:w-[300px]">
               Brand:
               <span class="font-bold">
                 {{ product.brand }}
               </span>
             </p>
-            <p class="w-[300px]">
-              Price:
+            <p class="md:w-[300px]">
+              Unit price:
               <span class="font-bold"> ${{ product.price }} </span>
             </p>
-            <p class="w-[300px]">
+            <p class="md:w-[300px]">
               Sum:
               <span class="font-bold"> ${{ product.price * product.quantity }} </span>
             </p>
@@ -38,17 +38,17 @@
         </div>
       </div>
       <div class="font-bold self-center my-4">
-        <p>
+        <p class="text-xl">
           Item total: <span class="text-green-700">${{ productStore.totalItemsPrice }}</span>
         </p>
-        <p>
+        <p class="text-xl">
           Delivery cost:
           <span class="text-green-700">{{ deliveryCost(productStore.totalItemsPrice) }}</span>
           <span class="block text-sm font-normal italic"
             >Delivery cost: $0 for orders above $250, otherwise $16</span
           >
         </p>
-        <p>
+        <p class="text-xl">
           Total price:
           <span class="text-green-700"
             >${{ productStore.totalItemsPrice + deliveryCost(productStore.totalItemsPrice) }}</span
