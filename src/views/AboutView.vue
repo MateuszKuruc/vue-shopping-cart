e708e6d36c2db3fc0c680305b66fdd31e0a15188
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-27 13:40:26 +0100

    style about page for desktop

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,69 +1,71 @@
 <template>
-  <div class="flex flex-col text-center my-8 px-4">
-    <h1 class="uppercase text-3xl pb-2">Feniks Shop</h1>
-    <h2 class="text-2xl italic text-green-500">
-      Welcome to Feniks Shop - Your Ultimate Tech Destination!
-    </h2>
-    <p class="text-left pt-4">
-      At Feniks Shop, we pride ourselves on being the go-to destination for cutting-edge technology
-      products. Whether you're a tech enthusiast, a professional on the go, or someone looking to
-      upgrade their digital lifestyle, we've got you covered.
-    </p>
-    <p class="text-left pt-4">
-      Explore a vast array of top-notch gadgets, from sleek laptops to powerful smartphones, premium
-      headphones, and a wide range of accessories that elevate your tech experience.
-    </p>
-    <h3 class="text-2xl text-green-500 pt-8">Why Feniks Shop?</h3>
-    <ul>
-      <li>
-        <h4 class="text-lg font-bold pt-4">Unparalleled Selection</h4>
-        <p class="text-left pt-4">
-          Our curated collection features the latest and greatest in technology. Discover a diverse
-          range of laptops, phones, headphones, and accessories from leading brands, ensuring you
-          have access to the best products in the market.
-        </p>
-      </li>
-      <li>
-        <h4 class="text-lg font-bold pt-4">Quality Assurance</h4>
-        <p class="text-left pt-4">
-          At Feniks Shop, quality is non-negotiable. We source products from reputable
-          manufacturers, guaranteeing the durability, performance, and reliability of every item we
-          offer. Your satisfaction is our top priority.
-        </p>
-      </li>
-      <li>
-        <h4 class="text-lg font-bold pt-4">Expert Guidance</h4>
-        <p class="text-left pt-4">
-          Not sure which laptop suits your needs or which headphones provide the best sound quality?
-          Our knowledgeable team is here to assist you. Benefit from expert advice to make informed
-          decisions and find the perfect tech companion.
-        </p>
-      </li>
-      <li>
-        <h4 class="text-lg font-bold pt-4">User-Friendly experience</h4>
-        <p class="text-left pt-4">
-          Navigating through our website is a breeze. Enjoy a seamless shopping experience with
-          intuitive design, detailed product information, and secure payment options. Feniks Shop is
-          designed with you in mind.
-        </p>
-      </li>
-      <li>
-        <h4 class="text-lg font-bold pt-4">Customer Satisfaction</h4>
-        <p class="text-left pt-4">
-          Our commitment to customer satisfaction extends beyond the purchase. Experience excellent
-          post-purchase support, timely deliveries, and hassle-free returns. Feniks Shop is not just
-          a store; it's a service tailored to your tech needs.
-        </p>
-      </li>
-    </ul>
-    <h2 class="text-2xl uppercase italic text-green-500 pt-8">Check Feniks' quality today</h2>
-    <p class="text-left pt-4">
-      Discover the world of possibilities that technology offers at Feniks Shop. We believe in
-      making the latest innovations accessible to everyone.
-    </p>
-    <p class="text-left pt-4">
-      Join us on this tech journey, and let Feniks Shop be your trusted partner in staying ahead of
-      the curve. Welcome to the future of technology!
-    </p>
-  </div>
+  <section class="flex items-center justify-center">
+    <div class="flex flex-col text-center my-8 px-4 max-w-[1400px]">
+      <h1 class="uppercase text-3xl pb-2">Feniks Shop</h1>
+      <h2 class="text-2xl italic text-green-500">
+        Welcome to Feniks Shop - Your Ultimate Tech Destination!
+      </h2>
+      <p class="text-left pt-4">
+        At Feniks Shop, we pride ourselves on being the go-to destination for cutting-edge
+        technology products. Whether you're a tech enthusiast, a professional on the go, or someone
+        looking to upgrade their digital lifestyle, we've got you covered.
+      </p>
+      <p class="text-left pt-4">
+        Explore a vast array of top-notch gadgets, from sleek laptops to powerful smartphones,
+        premium headphones, and a wide range of accessories that elevate your tech experience.
+      </p>
+      <h3 class="text-2xl text-green-500 pt-8">Why Feniks Shop?</h3>
+      <ul>
+        <li>
+          <h4 class="md:text-left text-lg font-bold pt-4">Unparalleled Selection</h4>
+          <p class="text-left pt-4">
+            Our curated collection features the latest and greatest in technology. Discover a
+            diverse range of laptops, phones, headphones, and accessories from leading brands,
+            ensuring you have access to the best products in the market.
+          </p>
+        </li>
+        <li>
+          <h4 class="md:text-left text-lg font-bold pt-4">Quality Assurance</h4>
+          <p class="text-left pt-4">
+            At Feniks Shop, quality is non-negotiable. We source products from reputable
+            manufacturers, guaranteeing the durability, performance, and reliability of every item
+            we offer. Your satisfaction is our top priority.
+          </p>
+        </li>
+        <li>
+          <h4 class="md:text-left text-lg font-bold pt-4">Expert Guidance</h4>
+          <p class="text-left pt-4">
+            Not sure which laptop suits your needs or which headphones provide the best sound
+            quality? Our knowledgeable team is here to assist you. Benefit from expert advice to
+            make informed decisions and find the perfect tech companion.
+          </p>
+        </li>
+        <li>
+          <h4 class="md:text-left text-lg font-bold pt-4">User-Friendly experience</h4>
+          <p class="text-left pt-4">
+            Navigating through our website is a breeze. Enjoy a seamless shopping experience with
+            intuitive design, detailed product information, and secure payment options. Feniks Shop
+            is designed with you in mind.
+          </p>
+        </li>
+        <li>
+          <h4 class="md:text-left text-lg font-bold pt-4">Customer Satisfaction</h4>
+          <p class="text-left pt-4">
+            Our commitment to customer satisfaction extends beyond the purchase. Experience
+            excellent post-purchase support, timely deliveries, and hassle-free returns. Feniks Shop
+            is not just a store; it's a service tailored to your tech needs.
+          </p>
+        </li>
+      </ul>
+      <h2 class="text-2xl uppercase italic text-green-500 pt-8">Check Feniks' quality today</h2>
+      <p class="text-left pt-4">
+        Discover the world of possibilities that technology offers at Feniks Shop. We believe in
+        making the latest innovations accessible to everyone.
+      </p>
+      <p class="text-left pt-4">
+        Join us on this tech journey, and let Feniks Shop be your trusted partner in staying ahead
+        of the curve. Welcome to the future of technology!
+      </p>
+    </div>
+  </section>
 </template>
