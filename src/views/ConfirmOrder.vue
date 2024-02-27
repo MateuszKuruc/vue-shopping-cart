8da7b6d614b48c54c6126e780b1b49fa168ebea1
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2024-02-27 16:21:58 +0100

    adjust confirmation page for different desktop screen sizes

--- src/views/ConfirmOrder.vue
+++ src/views/ConfirmOrder.vue
@@ -4,33 +4,33 @@
     <div class="text-center" v-if="!cart.length">
       <h1 class="text-3xl font-bold md:mt-0 mt-14">The cart is empty</h1>
     </div>
-    <div v-else class="flex flex-col xl:w-[1600px] md:w-auto mt-8 md:pb-8 pb-8">
+    <div v-else class="flex flex-col xl:max-w-[1600px] md:w-auto mt-8 md:pb-8 pb-8">
       <div class="md:pb-2 pb-4" v-for="product in cart" :key="product.id">
         <div
           class="flex flex-row shadow-2xl rounded-xl md:p-2 p-2 md:gap-0 md:mx-8 border-2 border-green-500"
         >
-          <div class="flex md:flex-row flex-col w-[100%] md:gap-8 gap-1">
-            <p class="md:w-[300px]">
+          <div class="flex md:flex-row flex-col w-[100%] md:gap-8 gap-1 md:justify-between">
+            <p class="md:w-[400px]">
               Item:
               <span class="font-bold">
                 {{ product.title }}
               </span>
             </p>
-            <p class="md:w-[300px]">
+            <p class="xl:w-[200px] md:w-[auto] xl:block md:hidden">
               Brand:
               <span class="font-bold">
                 {{ product.brand }}
               </span>
             </p>
-            <p class="md:w-[300px]">
+            <p class="xl:w-[200px] md:w-[auto]">
               Unit price:
               <span class="font-bold"> ${{ product.price }} </span>
             </p>
-            <p class="md:w-[300px]">
+            <p class="xl:w-[200px] md:w-[auto]">
               Sum:
               <span class="font-bold"> ${{ product.price * product.quantity }} </span>
             </p>
-            <p>
+            <p class="md:w-[100px]">
               Quantity:
               <span class="font-bold"> {{ product.quantity }} </span>
             </p>
